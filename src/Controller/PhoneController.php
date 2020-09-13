9c551f5c596b82b04a7a79fdd131c0b06501f0a1
Francis Libs <fr.libs@gmail.com>
2020-09-13 11:19:35 +0200

    Creation of the list of phones. Close #6

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Phone;
 use App\Repository\PhoneRepository;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,11 +17,6 @@ class PhoneController extends AbstractController
      */
     public function index(PhoneRepository $phoneRepository, SerializerInterface $serializer)
     {
-        $phones = $phoneRepository->findAll();
-        $data = $serializer->serialize($phones, 'json');
-
-        return new Response($data, 200, [
-            'Content-Type' => 'application/json'
-        ]);
+        return $this->json($phoneRepository->findAll(), 200, [], []);
     }
 }
